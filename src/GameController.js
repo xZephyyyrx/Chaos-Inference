4f8b1802ab03ac726dc2d5fe2cd6e321fba12a31
xZephyyyrx <samualreeves93@gmail.com>
2026-05-22 20:18:11 +1200

    Tileset Update
    New, simpler tileset created and implemented

--- src/GameController.js
+++ src/GameController.js
@@ -43,11 +43,11 @@ export default class GameController {
 
     // TEST DATA FOR LOADING & RENDERING MAPS //
     async loadTestData() {
-        this.#gridmap = await this.#dataloader.importGridmap('level1grid');
+        this.#gridmap = await this.#dataloader.importGridmap('thetube');
         this.#gridmap = this.#dataloader.parseMapData(this.#gridmap);
 
         try {
-            this.#fgTileset = await this.#dataloader.importTileset('appearancetestbright');
+            this.#fgTileset = await this.#dataloader.importTileset('appearancetestscaffold');
         } catch (error) {
             console.log(error);
         }
